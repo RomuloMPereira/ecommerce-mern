82be2bcc65131bf4fe2f4efe03ec297e19580e4e
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-01-17 16:19:59 -0300

    Login Endpoint/ Add JWT

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
@@ -21,6 +22,43 @@ router.post("/register", async (req, res) => {
         res.status(500).json(err);
     }
 
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne(
+            {
+                username: req.body.username
+            }
+        );
+
+        !user && res.status(401).json("Wrong Username");
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        originalPassword != req.body.password && res.status(401).json("Wrong Password");
+
+        const accessToken = jwt.sign(
+            {
+                id: user._id,
+                isAdmin: user.isAdmin,
+            },
+            process.env.JWT_SEC,
+            { expiresIn: "3d" }
+        );
+
+        const { password, ...others } = user._doc;
+
+        res.status(200).json({ ...others, accessToken });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
