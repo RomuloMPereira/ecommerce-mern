eabbeba56a4db6047f3bf2f24fda05bbedf15b6c
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-01-17 15:22:49 -0300

    User Register Endpoint

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -15,6 +17,7 @@ mongoose.connect(process.env.CONNECTION_STRING)
     });
 
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
