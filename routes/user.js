d80f4b42e01daef3d9c74a89a485784dc066ee5b
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-01-18 18:46:47 -0300

    Get User by ID (GET Endpoint)

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const { verifyTokenAndAuthorization } = require("./verifyToken");
+const { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 const User = require("../models/User");
 
 //UPDATE
@@ -31,6 +31,17 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
     try {
         await User.findByIdAndDelete(req.params.id);
         res.status(200).json("User has been deleted...");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
     } catch (err) {
         res.status(500).json(err);
     }
