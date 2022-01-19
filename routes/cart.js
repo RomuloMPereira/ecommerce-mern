982e5e921f55454607326260f6b71c3ca030ccd9
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-01-19 14:19:59 -0300

    Create Cart (POST Endpoint)

---
+++ routes/cart.js
@@ -0,0 +1,18 @@
+const Cart = require("../models/Cart");
+const { verifyToken } = require("./verifyToken");
+
+const router = require("express").Router();
+
+//CREATE
+router.post("/", verifyToken, async (req, res) => {
+    const newCart = new Cart(req.body);
+
+    try {
+        const savedCart = await newCart.save();
+        res.status(200).json(savedCart);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
