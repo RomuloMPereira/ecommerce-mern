5f4d1820eae9fcdda325621426d4e10d27eefce7
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-01-19 14:45:48 -0300

    Update Cart (PUT Endpoint)

--- routes/cart.js
+++ routes/cart.js
@@ -1,5 +1,5 @@
 const Cart = require("../models/Cart");
-const { verifyToken } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorization2 } = require("./verifyToken");
 
 const router = require("express").Router();
 
@@ -10,6 +10,27 @@ router.post("/", verifyToken, async (req, res) => {
     try {
         const savedCart = await newCart.save();
         res.status(200).json(savedCart);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//UPDATE
+router.put("/:id", verifyToken, async (req, res) => {
+    try {
+        const cart = await Cart.findOne({ id: req.params.id });
+        if (req.user.id === cart.userId || req.user.isAdmin) {
+            const updatedCart = await Cart.findByIdAndUpdate(
+                cart.id,
+                {
+                    $set: req.body,
+                },
+                { new: true }
+            );
+            res.status(200).json(updatedCart);
+        } else {
+            res.status(401).json("You are not authenticated!");
+        }
     } catch (err) {
         res.status(500).json(err);
     }
